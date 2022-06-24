9eb6230cf82d5baa0a770bff19ada32af5eaa397
hxnghia <hxnghia99@gmail.com>
2022-06-24 22:44:54 +0900

    change the total epochs

--- YOLOv4-for-studying/YOLOv4_config.py
+++ YOLOv4-for-studying/YOLOv4_config.py
@@ -82,9 +82,9 @@ TRAIN_SAVE_BEST_ONLY            = True  # saves only best model according valida
 TRAIN_SAVE_CHECKPOINT           = False # saves all best validated checkpoints in training process (may require a lot disk space) (False recommended)
 TRAIN_LOAD_IMAGES_TO_RAM        = False
 TRAIN_WARMUP_EPOCHS             = 2
-TRAIN_EPOCHS                    = 50
-TRAIN_LR_END                    = 1e-7
-TRAIN_LR_INIT                   = 1e-5
+TRAIN_EPOCHS                    = 40
+TRAIN_LR_END                    = 1e-8
+TRAIN_LR_INIT                   = 1e-6
 YOLO_LOSS_IOU_THRESHOLD         = 0.5
 
 
